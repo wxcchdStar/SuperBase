44a5de242a3821c87188069d85bd827cbd4ee912
chenhaidong <chenhaidong1@xiaomi.com>
2019-02-01 11:26:09 +0800

    add DoraemonKit

--- app/src/main/java/wxc/android/base/demo/DemoApplication.java
+++ app/src/main/java/wxc/android/base/demo/DemoApplication.java
@@ -2,11 +2,13 @@ package wxc.android.base.demo;
 
 import android.app.Application;
 
+import com.didichuxing.doraemonkit.DoraemonKit;
 import com.github.moduth.blockcanary.BlockCanary;
 import com.github.moduth.blockcanary.BlockCanaryContext;
 import com.squareup.leakcanary.LeakCanary;
 
 import timber.log.Timber;
+import wxc.android.base.app.activity.CommonWebViewActivity;
 
 public class DemoApplication extends Application {
 
@@ -19,9 +21,13 @@ public class DemoApplication extends Application {
             // init BlockCanary
             BlockCanary.install(this, new BlockCanaryContext()).start();
         }
-        // init
+        // init LeakCanary
         if (!LeakCanary.isInAnalyzerProcess(this)) {
             LeakCanary.install(this);
         }
+        // init
+        DoraemonKit.install(this);
+        DoraemonKit.setWebDoorCallback(s -> CommonWebViewActivity.goTo(DemoApplication.this, s));
     }
+
 }
